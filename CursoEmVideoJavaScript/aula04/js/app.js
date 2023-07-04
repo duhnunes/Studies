fe42efd4452c1056590b2ac36b84c105bf09defb
DuHNunes <duhnunes.dev@gmail.com>
2023-07-04 09:49:22 -0300

    Add Class 06

--- CursoEmVideoJavaScript/aula04/js/app.js
+++
@@ -1,3 +0,0 @@
-window.alert('Minha primeira mensagem'); // Mensagem de boas vindas
-window.confirm('Está gostando de JS?'); // Pergunta com confirmação
-window.prompt('Qual é seu nome?'); // Abre uma caixa de texto
