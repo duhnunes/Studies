68e67ffbd3ee165b2bcf159f4d9167b7137425e4
DuHNunes <duhnunes.dev@gmail.com>
2023-07-17 08:55:03 -0300

    Create Function JS to - * /

--- CursoEmVideoJavaScript/aula10/exercicio/js/app.js
+++ CursoEmVideoJavaScript/aula10/exercicio/js/app.js
@@ -1,9 +1,32 @@
+var tn1 = document.getElementById('txtn1');
+var tn2 = document.getElementById('txtn2');
+var res = document.getElementById('res');
+
+
 function somar(){
-    var tn1 = document.getElementById('txtn1');
-    var tn2 = document.getElementById('txtn2');
-    var res = document.getElementById('res');
     var n1 = Number(tn1.value);
     var n2 = Number(tn2.value);
-    var s = n1 + n2;
-    res.innerHTML = `Resultado: ${s}`;
+    var r = n1 + n2;
+    res.innerHTML = `Resultado: ${r}`;
+}
+
+function sub(){
+    var n1 = Number(tn1.value);
+    var n2 = Number(tn2.value);
+    var r = n1 - n2;
+    res.innerHTML = `Resultado: ${r}`;
+}
+
+function mult(){
+    var n1 = Number(tn1.value);
+    var n2 = Number(tn2.value);
+    var r = n1 * n2;
+    res.innerHTML = `Resultado: ${r}`;
+}
+
+function div(){
+    var n1 = Number(tn1.value);
+    var n2 = Number(tn2.value);
+    var r = n1 / n2;
+    res.innerHTML = `Resultado: ${r}`;
 }
