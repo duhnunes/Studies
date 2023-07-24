f291ce8e3ba88e65479006d079318edbe5a98c9a
DuHNunes <duhnunes.dev@gmail.com>
2023-07-24 17:49:39 -0300

    Upgrade code

--- CursoEmVideoJavaScript/aula12ex/ex014/script.js
+++ CursoEmVideoJavaScript/aula12ex/ex014/script.js
@@ -1,19 +1,21 @@
 function carregar() {
     var msg = document.getElementById('msg');
+    var msg2 = document.getElementById('msg2');
     var img = document.getElementById('imagem');
     var data = new Date();
     var hora = data.getHours();
-    msg.innerHTML = `Agora são: ${hora} horas..`;
+    var minutes = data.getMinutes();
+    msg.innerHTML = `Agora são exatamente: ${hora}:${minutes}`;
     if (hora >= 0 && hora < 12){
-        // BOM DIA
+        msg2.innerHTML = 'Bom Dia';
         img.src = 'fotomanha.jpg';
         document.body.style.background = '#e2cd9f';
     } else if (hora >= 12 && hora < 18){
-        // BOA TARDE
+        msg2.innerHTML = 'Boa Tarde';
         img.src = 'fototarde.jpg';
         document.body.style.background = '#b9846f';
     } else {
-        // BOA NOITE
+        msg2.innerHTML = 'Boa Noite';
         img.src = 'fotonoite.jpg';
         document.body.style.background = '#515154';
     }
